ee1436cf7838a4605e4a11afebf500fdac1fc250
Omgeta <anooptiger@Hotmail.com>
2020-08-05 17:15:57 +0800

    Update index.js

--- SP/hangman/index.js
+++ SP/hangman/index.js
@@ -1,13 +1,37 @@
-const {WordCollection} = require("./hangman.js")
-const wordlist = "./words.txt"
+/*
+Author: Omgeta
+Date: 4/8/2020
+Description: Hangman Game
+*/
 
+const fs = require("fs");
+const readlineSync = require("readline-sync");
 
-let game = new WordCollection(wordlist)
-game.start()
-while (!game.winner) {
-	game.display()
-	let input = game.prompt()
-	game.update(input)
-	game.nextRound()
+const { WordCollection } = require("./hangman.js");
+const wordlists = fs.readdirSync("./wordlists").filter(file => file.endsWith(".csv"));
+function wordlistsString() {
+	return wordlists.map(e => e.split(".").slice(0, -1).join(".").toUpperCase()).join(", ");
 }
-console.log("=======\nResults\n=======\nScore: "+game.score)
+
+const name = readlineSync.question("Enter your name: ").trim();
+let running = true;
+while (running) {
+	const category = readlineSync.question(`Pick a wordlist (${wordlistsString()}): `).trim().toLowerCase();
+	const wordlist = wordlists.find(e => e == category.concat(".csv"));
+	if (!wordlist) continue;
+	const game = new WordCollection("./wordlists".concat(wordlist));
+
+	game.start(name);
+	while (!game.winner) {
+		game.display();
+		const input = game.prompt();
+		game.update(input);
+		game.nextRound();
+	}
+	game.win();
+
+	const restart = readlineSync.question("Would you like to try again (Y/N): ").trim().charAt(0).toUpperCase();
+	if (!restart == "Y") {
+		running = false;
+	}
+}
