17f921111e3484af8b3cba1f0e668b7aaca46a3e
Omgeta <anooptiger@hotmail.com>
2020-09-17 21:23:08 +0800

    recursive added

--- NYJC/Exam_Practice/Search_Algorithms/BinarySearch.py
+++ NYJC/Exam_Practice/Search_Algorithms/BinarySearch.py
@@ -1,12 +1,25 @@
 def binary_search(array, value):
     start, end = 0, len(array)-1
-    mid = (start + end)//2
+    mid = (start + end) // 2
 
     while start != end:
         if array[mid] >= value:
             end = mid
         else:
             start = mid + 1
-        mid = (start + end)//2
+        mid = (start + end) // 2
 
     return mid
+
+def recursive_search(array, value, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+
+    if array[mid] == value:
+        return mid
+    elif array[mid] > value:
+        return recursive_search(array, value, start, mid)
+    else:
+        return recursive_search(array, value, mid+1, end)
