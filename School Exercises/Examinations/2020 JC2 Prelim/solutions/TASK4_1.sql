8cb5ac235329bf4a46e905d2ee7193c6ad870c94
Anoop Felix <anooptiger@hotmail.com>
2021-08-17 22:08:21 +0800

    added 2020 prelim incomplete

---
+++ School Exercises/Examinations/2020 JC2 Prelim/solutions/TASK4_1.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS Registration;
+DROP TABLE IF EXISTS Arts;
+DROP TABLE IF EXISTS Cultural;
+DROP TABLE IF EXISTS Sports;
+CREATE TABLE IF NOT EXISTS Registration (
+    StudentID INTEGER,
+    Type TEXT,
+    Venue TEXT
+    Session TEXT,
+    PRIMARY KEY (StudentID)
+);
+CREATE TABLE IF NOT EXISTS Arts (
+    StudentID INTEGER,
+    Performance TEXT CHECK(Performance in ("True", "False")),
+    PRIMARY KEY (StudentID),
+    FOREIGN KEY (StudentID) REFERENCES Registration(StudentID)
+);
+CREATE TABLE IF NOT EXISTS Cultural (
+    StudentID INTEGER,
+    Race TEXT,
+    PRIMARY KEY (StudentID),
+    FOREIGN KEY (StudentID) REFERENCES Registration(StudentID)
+);
+CREATE TABLE IF NOT EXISTS Sports (
+    StudentID INTEGER,
+    Contact TEXT CHECK(Contact in ("NC", "C")),
+    Cost REAL,
+    PRIMARY KEY (StudentID),
+    FOREIGN KEY (StudentID) REFERENCES Registration(StudentID)
+);
