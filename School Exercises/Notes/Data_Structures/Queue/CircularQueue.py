ab9bf22658ff34eff23983125401fb4914ed1e38
Anoop Felix <anooptiger@hotmail.com>
2021-11-14 17:15:57 +0800

    fix

--- School Exercises/Notes/Data_Structures/Queue/CircularQueue.py
+++ School Exercises/Notes/Data_Structures/Queue/CircularQueue.py
@@ -6,37 +6,37 @@ class CircularQueue:
         self.size = size
         self.data = [None] * self.size
         self.head = None
+        self.tail = None
 
     def is_empty(self):
         """ Checks if the queue is empty """
         return self.head is None
 
     def enqueue(self, value: any):
-        """ Enqueues an element to the end of the queue, overwriting the oldest elements if max size is reached """
-        if self.is_empty():
+        """ Enqueues an element to the next free spot in the queue """
+        if (self.tail + 1) % self.size == self.front:
+            print("Queue is full")
+        elif self.is_empty():
             self.head = 0
+            self.tail = 0
+            self.data[self.tail] = value
         else:
-            # increment head and wrap around if too large
-            self.head = (self.head + 1) % self.size
-
-        self.data[self.head] = value
+            self.tail = (self.tail + 1) % self.size
+            self.data[self.tail] = value
 
     def dequeue(self) -> any:
         """ Dequeues an element from the front of the queue """
         if self.is_empty():
             print("Queue is empty. Cannot pop any more elements")
-            return None
-
-        # get value and set it to None for GC to delete it
-        value = self.data[self.head]
-        self.data[self.head] = None
-
-        # decrement head and wrap around if too small
-        self.head = (self.head - 1) % self.size
-
-        # revert queue to empty state if there are no more elements left in the whole queue
-        if self.data[self.head] is None:
+        elif self.head == self.tail:
+            res = self.data[self.head]
             self.head = None
+            self.tail = None
+            return res
+        else:
+            res = self.queue[self.head]
+            self.head = (self.head + 1) % self.size
+            return res
 
     def peek(self) -> any:
         """ Peeks at an element from the front of the queue """
